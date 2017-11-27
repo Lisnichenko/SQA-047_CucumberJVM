37841790dba3c95f3920c15ff4088ed3eef0b533
Alex Zvonov <gambelz@gmail.com>
2017-11-27 22:57:24 +0100

    second scenario added

--- selenide/src/test/java/stepDefinition/WhenUserAddNewRecord.java
+++ selenide/src/test/java/stepDefinition/WhenUserAddNewRecord.java
@@ -1,10 +1,13 @@
 package stepDefinition;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import model.AngularPage;
+
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -18,18 +21,29 @@ public class WhenUserAddNewRecord {
         angularPage.open();
     }
 
-    @When("^user add new task (.*)$")
+    @When("^user add new task \"(.*)\"$")
     public void user_add_new_task(String taskName){
         angularPage.addTask(taskName);
     }
 
-    @Then("^(.*) should appear in the list")
-    public void appleShouldAppearInTheList(String taskName) {
+    @Then("^\"(.*)\" should appear in the list")
+    public void itemShouldAppearInTheList(String taskName) {
         assertTrue(angularPage.isItemPresentInList(taskName));
     }
 
-    @And("^remove (.*)$")
+    @Then("^tasks should appear in the list")
+    public void tasksShouldAppearInTheList(DataTable dataTable) {
+        dataTable.asList(String.class).forEach(this::itemShouldAppearInTheList);
+    }
+
+    @And("^remove \"(.*)\"$")
     public void removeApple(String taskName){
       angularPage.removeTask(taskName);
+    }
+
+    @When("^user adds new task$")
+    public void user_adds_new_task(DataTable dataTable) {
+        List<String> data = dataTable.asList(String.class);
+        data.forEach(this::user_add_new_task);
     }
 }
--- selenide/src/test/resources/features/WhenUserAddNewRecord.feature
+++ selenide/src/test/resources/features/WhenUserAddNewRecord.feature
@@ -5,6 +5,16 @@ Feature: When User Add New Record
     
 Scenario: Adding apple to the list
     Given user is on the angular page
-    When user add new task Apple
-    Then Apple should appear in the list
-    And remove Apple
+    When user add new task "Apple"
+    Then "Apple" should appear in the list
+    And remove "Apple"
+
+
+Scenario: Adding multiple records to the list
+    Given user is on the angular page
+    When user adds new task
+    |Apple|Grape|Peach|
+    Then tasks should appear in the list
+    |Apple|Grape|Peach|
+
+
